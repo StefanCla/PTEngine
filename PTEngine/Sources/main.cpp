107111ebedda6a7766cac02c58f967a3877dee41
StefanCla <stefanclaasse@outlook.com>
2025-02-13 22:47:11 +0000

    [FIX] Fixed up issues that broke libraries

--- PTEngine/Sources/main.cpp
+++ PTEngine/Sources/main.cpp
@@ -1,8 +1,28 @@
 #include <cstdio>
+#include <SDL3/SDL.h>
+#include "slikenet/MessageIdentifiers.h"
+#include "slikenet/peerinterface.h"
+#include "slikenet/Gets.h"
+#include "slikenet/Kbhit.h"
+
+static SDL_Window* Window = nullptr;
+static SDL_Renderer* Renderer = nullptr;
 
 int main()
 {
-	std::printf("Hello World\n");
+	SLNet::RakPeerInterface* Interface = SLNet::RakPeerInterface::GetInstance();
+	
+	if (Interface)
+	{
+		printf("SLikeNet seems to be working\n");
+	}
+
+	if (SDL_CreateWindowAndRenderer("PTEngine", 640, 480, 0, &Window, &Renderer))
+	{
+		printf("SDL Seems to be working\n");
+	}
+
+	printf("Hello World\n");
 
 	return 0;
 }
